30914dbfd62ee4cf20cb1319ab861cab9030d6ad
sebastian-egg <sebastian.egglseder@gmx.de>
2026-08-23 17:17:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const C="grunwald-v03";const A=["./","./index.html","./manifest.webmanifest","./assets/hero.png","./assets/wolf.png","./assets/grass.jpg","./assets/path.jpg","./assets/whirlwind.png","./assets/charge.png","./assets/portrait.png","./assets/inventory.png","./assets/character.png","./assets/quests.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="grunwald-v04";
+const A=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
